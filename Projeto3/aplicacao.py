fb94eaf12ea146f75d73040c906fbe7c50832463
unknown <you@example.com>
2019-08-23 17:54:32 -0300

    Projeto3

--- Projeto3/aplicacao.py
+++ Projeto3/aplicacao.py
@@ -33,78 +33,82 @@ def client():
     #     payload = bytearray(f)
     #     print('teste3')
         
-    f = bytes([0x00])*5 + bytes([0xf1]) + bytes([0xf2]) + bytes([0xf3]) + bytes([0x00])*5 + bytes([0xf1]) + bytes([0xf2]) + bytes([0xf3]) + bytes([0x00])*5
+    f = bytes([0x00])*45 + bytes([0xf1]) + bytes([0xf2]) + bytes([0xf3]) + bytes([0x00])*45 + bytes([0xf1]) + bytes([0xf2]) + bytes([0xf3]) + bytes([0x00])*45
     payload = bytearray(f)
-    print('teste3')
-            
-
     eop = bytes([0xf1]) + bytes([0xf2]) + bytes([0xf3])
     eopReplaced = bytes([0x00]) + bytes([0xf1]) +  bytes([0x00]) + bytes([0xf2]) +  bytes([0x00]) + bytes([0xf3])
     
-    payload =  payload.replace(eop, eopReplaced)
 
-    emptyHead = bytes([0x00]) * 6
-    imgSize = len(payload).to_bytes(4,"big")
-    head = imgSize + emptyHead
+    eachPayload = [f[x:x+128] for x in range(0, len(f), 128)]
+ 
+    totalPackage = len(eachPayload).to_bytes(2,"big")
+    numberPackage = 0
+    emptyHead =  bytes([0x00]) * 3
     
-    package = head + payload + eop
     
-
-
-
-    # Inicializa enlace ... variavel com possui todos os metodos e propriedades do enlace, que funciona em threading
-    com = enlace(serialName) # repare que o metodo construtor recebe um string (nome)
-    # Ativa comunicacao
-    com.enable()
-
-    # Log
-    print("-------------------------")
-    print("Comunicação inicializada")
-    print("  porta : {}".format(com.fisica.name))
-    print("-------------------------")
-
-    # Carrega dados
-    print ("gerando dados para transmissao :")
-
-    # Transmite dado
-    print("tentado transmitir .... {} bytes".format(len(package)))
-    start = time.time()
-    com.sendData(package)
-
-    #espera o fim da transmissão
-    while(com.tx.getIsBussy()):
-       pass
-
-    # Atualiza dados da transmissão
-    txSize = com.tx.getStatus()
-
-    print ("Transmitido {} bytes ".format(txSize))
-
-    response, responseSize = com.getData(1)
-
-    if response == bytes([0xa1]):
-        print("ERRO: EOP não encontrado")
-    if response == bytes([0xa2]):
-       print("ERRO: EOP está no lugar errado")
-    if response == bytes([0xa3]):
-        print("Sucesso")
-   
-    end = time.time()
+    for payloadS in eachPayload:
+        numberPackage += 1
+        payloadSize = len(payloadS).to_bytes(4,"big")
+        numberPackageB = numberPackage.to_bytes(1,"big")
+       
+        head = numberPackageB + totalPackage + payloadSize + emptyHead
+        package = head + payloadS + eop
         
+        payload =  payload.replace(eop, eopReplaced)
 
-    delta = end - start
-    ThroughPut = len(payload)/delta
-    OverHead = len(package)/len(payload)
+        # Inicializa enlace ... variavel com possui todos os metodos e propriedades do enlace, que funciona em threading
+        com = enlace(serialName) # repare que o metodo construtor recebe um string (nome)
+        # Ativa comunicacao
+        com.enable()
 
-    print("Tempo:      {} s".format(delta))
-    print("ThroughPut: {} b/s".format(ThroughPut))
-    print("OverHead:   {} %".format(OverHead))
+        # Log
+        print("-------------------------")
+        print("Comunicação inicializada")
+        print("  porta : {}".format(com.fisica.name))
+        print("-------------------------")
+
+        # Carrega dados
+        print ("gerando dados para transmissao :")
+
+        # Transmite dado
+        print("tentado transmitir .... {} bytes".format(len(package)))
+        start = time.time()
+        com.sendData(package)
+
+        #espera o fim da transmissão
+        while(com.tx.getIsBussy()):
+            pass
+
+        # Atualiza dados da transmissão
+        txSize = com.tx.getStatus()
+
+        print ("Transmitido {} bytes ".format(txSize))
+
+        response, responseSize = com.getData(1)
+
+        if response == bytes([0xa1]):
+            print("ERRO: EOP não encontrado")
+        if response == bytes([0xa2]):
+            print("ERRO: EOP está no lugar errado")
+        if response == bytes([0xa3]):
+            print("Sucesso")
     
-    # Encerra comunicação
-    print("-------------------------")
-    print("Comunicação encerrada")
-    print("-------------------------")
-    com.disable()
+        end = time.time()
+            
+
+        delta = end - start
+        ThroughPut = len(payload)/delta
+        OverHead = len(package)/len(payload)
+
+        print("Tempo:      {} s".format(delta))
+        print("ThroughPut: {} b/s".format(ThroughPut))
+        print("OverHead:   {} %".format(OverHead))
+        
+        # Encerra comunicação
+        print("-------------------------")
+        print("Comunicação encerrada")
+        print("-------------------------")
+        com.disable()
 
     #so roda o main quando for executado do terminal ... se for chamado dentro de outro modulo nao roda
 if __name__ == "__main__":
