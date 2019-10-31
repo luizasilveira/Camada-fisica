53b0b49e446519a09538ad4ab4e26e980f3e2afa
unknown <you@example.com>
2019-10-31 17:13:36 -0300

    projeto 7

--- Projeto7/decode_versaoAlunos.py
+++ Projeto7/decode_versaoAlunos.py
@@ -31,8 +31,8 @@ def main():
     # os seguintes parametros devem ser setados:
     
     sd.default.samplerate = freqDeAmostragem  #taxa de amostragem
-    sd.default.channels = 2  #voce pode ter que alterar isso dependendo da sua placa
-    duration = 1 #tempo em segundos que ira aquisitar o sinal acustico captado pelo mic
+    sd.default.channels = 1  #voce pode ter que alterar isso dependendo da sua placa
+    duration = 2 #tempo em segundos que ira aquisitar o sinal acustico captado pelo mic
 
 
     # faca um print na tela dizendo que a captacao comecará em n segundos. e entao 
@@ -61,8 +61,6 @@ def main():
     # use a funcao linspace e crie o vetor tempo. Um instante correspondente a cada amostra!
 
     t = np.linspace(0,duration,int(numAmostras))
-    print(len(dados))
-    print(len(t))
 
     #plot do gravico  áudio vs tempo!
     plt.plot(dados,t)
@@ -87,10 +85,84 @@ def main():
     #frequencia do sinal podem gerar mais de um pico, e na verdade tempos apenas 1.
    
     #index = peakutils.indexes(,,)
-    index = peakutils.indexes(yf, thres=0.5, min_dist=15)
-    print(index)
-    
     #printe os picos encontrados! 
+    index = peakutils.indexes(yf, thres=0.1, min_dist=50)
+    picos = [xf[i] for i in index if xf[i] > 600 and xf[i] < 1800]
+    maxi = max(picos, key=int)
+    minn = min(picos, key=int)
+    delta = 10
+    print("Picos : {}  ".format(picos))
+
+    tabela_DTMF = {"1":[1209, 697], "2":[1336, 697], "3":[1477, 697], "A":[1633, 697],
+         "4":[1209, 770], "5":[1336, 770], "6":[1477, 770], "B":[1633, 770],
+         "7":[1209, 852], "8":[1336, 852], "9":[1477, 852], "C":[1633, 852],
+         "X":[1209, 941], "0":[1336, 941], "#":[1477, 941], "D":[1633, 941]}
+
+    if 1209 + delta >= maxi >= 1209 - delta :
+        if 697 + delta >= minn >= 697 - delta:     
+            print("A tecla encontrada foi a : 1  ")  
+
+    if 1336 + delta >= maxi >= 1336 - delta:
+        if 697 + delta >= minn >= 697 - delta:   
+            print("A tecla encontrada foi a : 2  ")  
+
+    if 1477 + delta >=  maxi >= 1477 - delta:
+        if 697 + delta >= minn >= 697 - delta :   
+            print("A tecla encontrada foi a : 3  ")  
+
+
+    if 1633 + delta >= maxi >= 1633 - delta:
+        if 697 + delta >= minn >= 697 - delta :   
+            print("A tecla encontrada foi a : A  ")  
+
+    if 1209 + delta >= maxi >= 1209 - delta :
+        if 770 + delta >= minn >= 770 - delta :   
+            print("A tecla encontrada foi a : 4  ")  
+
+    if 1336 + delta >= maxi >= 1336 - delta :
+        if 770 + delta >= minn >= 770 - delta:   
+            print("A tecla encontrada foi a : 5  ")    
+
+    if 1477 + delta >= maxi >= 1477 - delta: 
+        if 770 + delta >= minn >= 770 - delta:   
+            print("A tecla encontrada foi a : 6  ")    
+
+    if 1633 + delta >= maxi >= 1633 - delta:
+        if 770 + delta >= minn >= 770 - delta :   
+            print("A tecla encontrada foi a : B  ")    
+
+    if 1209 + delta >= maxi >= 1209 - delta:
+        if 852 + delta >= minn >= 852 - delta :   
+            print("A tecla encontrada foi a : 7  ")
+
+    if 1336 + delta >= maxi >= 1336 - delta:
+        if 852 + delta >= minn >= 852 - delta :   
+            print("A tecla encontrada foi a : 8  ")
+
+    if 1477 + delta >= maxi >= 1477 - delta :
+        if 852 + delta >= minn >= 852 - delta :   
+            print("A tecla encontrada foi a : 9  ")    
+    if 1633 + delta >= maxi >= 1633 - delta :
+        if 852 + delta >= minn >= 852 - delta :   
+            print("A tecla encontrada foi a : C  ")    
+
+    
+    if 1209 + delta  >= maxi >= 1209 - delta :
+        if 941 + delta >= minn >= 941 - delta  :   
+            print("A tecla encontrada foi a : X  ")  
+    if 1336 + delta  >= maxi >= 1336 - delta:
+        if 941 + delta >= minn >= 941 - delta :   
+            print("A tecla encontrada foi a : 0  ")        
+    if 1477 + delta  >= maxi >= 1477 - delta:
+        if 941 + delta >= minn >= 941 - delta  :   
+            print("A tecla encontrada foi a : #  ")    
+    if 1633 + delta >= maxi >= 1633 - delta:
+        if 941 + delta >= minn >= 941 - delta  :   
+            print("A tecla encontrada foi a : D  ")    
+
+
+    
+    
     
     #encontre na tabela duas frequencias proximas às frequencias de pico encontradas e descubra qual foi a tecla
     #print a tecla.
